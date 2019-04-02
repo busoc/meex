23ef923c77100184aaa7a3e4373a2e2f61f9481d
nico <brun.nicolas21@gmail.com>
2019-04-02 13:59:45 +0200

    rename vrx to vmucat + implement diff and count

--- cmd/vmucat/main.go
+++ cmd/vmucat/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
+	"time"
 
-	"github.com/busoc/rt"
+	"github.com/busoc/meex"
 	"github.com/busoc/vmu"
 	"github.com/midbel/cli"
 	"github.com/midbel/linewriter"
@@ -73,16 +76,14 @@ func main() {
 }
 
 func runList(cmd *cli.Command, args []string) error {
-	keepInvalid := cmd.Flag.Bool("e", false, "invalid packet")
+	keepInvalid := cmd.Flag.Bool("e", false, "keep invalid packets")
 	if err := cmd.Flag.Parse(args); err != nil {
 		return err
 	}
-	mr, err := rt.Browse(cmd.Flag.Args(), true)
+	d, err := Decode(cmd.Flag.Args())
 	if err != nil {
 		return err
 	}
-
-	d := vmu.NewDecoder(rt.NewReader(mr))
 	line := linewriter.New(1024, 1)
 	seen := make(map[uint8]vmu.Packet)
 
@@ -116,6 +117,190 @@ func runList(cmd *cli.Command, args []string) error {
 			return err
 		}
 	}
+}
+func runCount(cmd *cli.Command, args []string) error {
+	keepInvalid := cmd.Flag.Bool("e", false, "keep invalid packets")
+	by := cmd.Flag.String("b", "", "count packets by channel or origin")
+	if err :=  cmd.Flag.Parse(args); err != nil {
+		return err
+	}
+	var (
+		getBy func(vmu.Packet) uint8
+		missBy func(vmu.Packet, vmu.Packet) uint64
+		appendBy func(*linewriter.Writer, uint8)
+	)
+	switch strings.ToLower(*by) {
+	case "channel", "":
+		getBy = byChannel
+		missBy = func(p, prev vmu.Packet) uint64 {
+			if p.VMUHeader.Sequence < prev.VMUHeader.Sequence {
+				return 0
+			}
+			return uint64(p.VMUHeader.Sequence - prev.VMUHeader.Sequence) - 1
+		}
+		appendBy = func(line *linewriter.Writer, c uint8) {
+			line.AppendBytes(whichChannel(c), 4, linewriter.Text | linewriter.AlignLeft)
+		}
+	case "origin":
+		getBy = byOrigin
+		missBy = func(p, prev vmu.Packet) uint64 {
+			if p.DataHeader.Counter < prev.DataHeader.Counter {
+				return 0
+			}
+			return uint64(p.DataHeader.Counter - prev.DataHeader.Counter) - 1
+		}
+		appendBy = func(line *linewriter.Writer, c uint8) {
+			line.AppendUint(uint64(c), 2, linewriter.AlignCenter | linewriter.Hex | linewriter.WithZero)
+		}
+	default:
+		return fmt.Errorf("unknown value %s", *by)
+	}
+
+	d, err := Decode(cmd.Flag.Args())
+	if err != nil {
+		return err
+	}
+	stats := make(map[uint8]meex.Coze)
+	seen := make(map[uint8]vmu.Packet)
+	for {
+		p, err := d.Decode(false)
+		switch err {
+		case nil, vmu.ErrInvalid:
+			by := getBy(p)
+			cz := stats[by]
+
+			cz.Count++
+			cz.Size += uint64(p.VMUHeader.Size)
+			if err == vmu.ErrInvalid {
+				cz.Error++
+				if !*keepInvalid {
+					continue
+				}
+			}
+			if prev, ok := seen[by]; ok {
+				cz.Missing += missBy(p, prev)
+			}
+			seen[by], stats[by] = p, cz
+		case vmu.ErrSkip:
+		case io.EOF:
+			line := linewriter.NewWriter(1024, 1, ' ')
+			for b, cz := range stats {
+				appendBy(line, b)
+				line.AppendUint(cz.Count, 6, linewriter.AlignRight)
+				line.AppendUint(cz.Missing, 6, linewriter.AlignRight)
+				line.AppendUint(cz.Error, 6, linewriter.AlignRight)
+				line.AppendUint(cz.Size, 6, linewriter.AlignRight)
+
+				os.Stdout.Write(append(line.Bytes(), '\n'))
+				line.Reset()
+			}
+			return nil
+		default:
+			return err
+		}
+	}
+}
+
+func runDiff(cmd *cli.Command, args []string) error {
+	keepInvalid := cmd.Flag.Bool("e", false, "keep invalid packets")
+	by := cmd.Flag.String("b", "", "count packets by channel or origin")
+	duration := cmd.Flag.Duration("d", time.Second, "maximum gap duration")
+	if err := cmd.Flag.Parse(args); err != nil {
+		return err
+	}
+
+	var (
+		getBy func(vmu.Packet) uint8
+		gapBy func(vmu.Packet, vmu.Packet) (bool, meex.Gap)
+		appendBy func(*linewriter.Writer, vmu.Packet)
+	)
+	switch strings.ToLower(*by) {
+	case "channel", "":
+		getBy = byChannel
+		gapBy = func(p, prev vmu.Packet) (ok bool, g meex.Gap) {
+			last, first := int(p.VMUHeader.Sequence), int(prev.VMUHeader.Sequence)
+			delta := p.VMUHeader.Timestamp().Sub(prev.VMUHeader.Timestamp())
+			if diff := last - first; diff != 1 && (*duration == 0 || delta <= *duration) {
+				g.Last = first
+				g.First = last
+				g.Starts = prev.VMUHeader.Timestamp()
+				g.Ends = p.VMUHeader.Timestamp()
+
+				ok = true
+			}
+			return
+		}
+		appendBy = func(line *linewriter.Writer, p vmu.Packet) {
+			line.AppendBytes(whichChannel(p.VMUHeader.Channel), 4, linewriter.Text | linewriter.AlignLeft)
+		}
+	case "origin":
+		getBy = byOrigin
+		gapBy = func(p, prev vmu.Packet) (ok bool, g meex.Gap) {
+			last, first := int(p.DataHeader.Counter), int(prev.DataHeader.Counter)
+			delta := p.DataHeader.Acquisition().Sub(prev.DataHeader.Acquisition())
+			if diff := last - first; diff != 1 && (*duration == 0 || delta <= *duration) {
+				g.Last = first
+				g.First = last
+				g.Starts = prev.DataHeader.Acquisition()
+				g.Ends = p.DataHeader.Acquisition()
+
+				ok = true
+			}
+			return
+		}
+		appendBy = func(line *linewriter.Writer, p vmu.Packet) {
+			line.AppendBytes(whichChannel(p.VMUHeader.Channel), 4, linewriter.Text | linewriter.AlignLeft)
+			line.AppendUint(uint64(p.DataHeader.Origin), 2, linewriter.AlignCenter | linewriter.Hex | linewriter.WithZero)
+		}
+	default:
+		return fmt.Errorf("unknown value %s", *by)
+	}
+
+	d, err := Decode(cmd.Flag.Args())
+	if err != nil {
+		return err
+	}
+	seen := make(map[uint8]vmu.Packet)
+	line := linewriter.NewWriter(1024, 1, ' ')
+	for {
+		p, err := d.Decode(false)
+		switch err {
+		case nil, vmu.ErrInvalid:
+			if err == vmu.ErrInvalid && !*keepInvalid {
+				continue
+			}
+			by := getBy(p)
+			if prev, ok := seen[by]; ok {
+				if ok, g := gapBy(p, prev); ok {
+					appendBy(line, p)
+					line.AppendTime(g.Starts, "2006-01-02 15:04:05.000", linewriter.AlignRight)
+					line.AppendTime(g.Ends, "2006-01-02 15:04:05.000", linewriter.AlignRight)
+					line.AppendInt(int64(g.Last), 8, linewriter.AlignRight)
+					line.AppendInt(int64(g.First), 8, linewriter.AlignRight)
+					line.AppendInt(int64(g.Missing()), 8, linewriter.AlignRight)
+					line.AppendString(g.Duration().String(), 10, linewriter.AlignRight)
+
+					os.Stdout.Write(append(line.Bytes(), '\n'))
+					line.Reset()
+				}
+			}
+			seen[by] = p
+		case vmu.ErrSkip:
+		case io.EOF:
+			return nil
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func Decode(files []string) (*vmu.Decoder, error) {
+	mr, err := meex.Browse(files, true)
+	if err != nil {
+		return nil, err
+	}
+	return vmu.NewDecoder(meex.NewReader(mr)), nil
 }
 
 func dumpPacket(line *linewriter.Writer, p vmu.Packet, missing uint32, valid bool) {
@@ -152,12 +337,13 @@ func dumpPacket(line *linewriter.Writer, p vmu.Packet, missing uint32, valid boo
 	os.Stdout.Write(append(line.Bytes(), '\n'))
 }
 
-func runDiff(cmd *cli.Command, args []string) error {
-	return nil
+
+func byChannel(p vmu.Packet) uint8 {
+	return p.VMUHeader.Channel
 }
 
-func runCount(cmd *cli.Command, args []string) error {
-	return nil
+func byOrigin(p vmu.Packet) uint8 {
+	return p.DataHeader.Origin
 }
 
 func whichChannel(c uint8) []byte {
